d4371a023d747c6070176ec0c882c51ddc095707
Pugachev85 <workpost2008@mail.ru>
2022-09-09 23:24:37 +0300

    Task_2 (remarks corrected)

--- src/Task_2/ComplexExamples.java
+++ src/Task_2/ComplexExamples.java
@@ -1,6 +1,5 @@
 package Task_2;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,21 +11,19 @@ public class ComplexExamples {
         [3, 4, 2, 7], 10 -> [3, 7] - вывести пару именно в скобках, которые дают сумму - 10
      */
     public static void main(String[] args) {
-        int[] array = {3, 4, 2, 7, 8};
+        List<Integer> list = Arrays.asList(3, null, 4, 2, 7, 8);
+        List<Integer> list2 = Arrays.asList(3, null, 4, 2, 9, 8);
         int searchedSum = 10;
-        System.out.println(findNums(array, searchedSum));
+        System.out.println(findNums(list, searchedSum));
+        System.out.println(findNums(list2, searchedSum));
     }
 
-    public static List<Integer> findNums(int[] array, int searchedSum) {
-        if (array == null) return null;
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i + 1; j < array.length; j++) {
-                if (array[i] + array[j] == searchedSum) {
-                    result.add(array[i]);
-                    result.add(array[j]);
-                    return result;
-                }
+    public static String findNums(List<Integer> list, int searchedSum) {
+        if (list == null) return null;
+        for (Integer curNum : list) {
+            if (curNum == null) continue;
+            if (list.contains(searchedSum-curNum)) {
+                return String.format("[%d,%d]", curNum, searchedSum-curNum);
             }
         }
         return null;
